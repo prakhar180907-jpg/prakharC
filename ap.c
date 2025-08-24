71ce4d1d3b02a08120a21f75225b883d249c52d3
prakhar180907-jpg <prakhar180907@gmail.com>
2025-08-24 10:37:40 +0530

    Add files via upload

---
+++ ap.c
@@ -0,0 +1,76 @@
+//A code to print using int by using %d
+/*# include<stdio.h>
+
+int main() {
+    int age=18;
+    printf("age is %d",age);
+    return 0;
+
+}*/
+
+//A code to print value using float %f
+/*# include<stdio.h>
+
+ int main()
+ {
+    float pi=3.14;
+    printf("pi is %f \n",pi);
+    return 0;
+ }*/
+
+//A code to print using char and %c variable
+/*# include<stdio.h>
+
+ int main()
+ {
+    char star='*';
+    printf("star is %c \n", star);
+    return 0;
+ }*/
+
+//A code to enter age using scanf
+ /*# include<stdio.h>
+   int main()
+   {
+    int age;
+    printf("Enter age");
+    scanf("%d", &age);
+    printf("age is: %d", age);
+    return 0;
+   }*/
+
+//A code to display sum of two numbers
+   # include<stdio.h>
+  int main()
+  {
+    int a, b;
+    printf("enter a");
+    scanf("%d", &a);
+    printf("enter b");
+    scanf("%d", &b);
+
+    int sum = a + b;
+    printf("sum is : %d", sum);
+    return 0;
+      
+  }
+
+//A code to display sum,diffrence,multiplication,division of two variables without using a third variable
+/* #include<stdio.h> 
+  int main()
+  {
+   int a, b;
+   printf("enter a");
+   scanf("%d", &a);
+   printf("enter b");
+   scanf("%d", &b );
+    
+   int sum=a + b;
+   printf("sum is: %d \n", a + b);
+   printf("diffrence is: %d \n", a - b);
+   printf("multiplication  is: %d \n", a * b);
+   printf("quotient is: %d \n", a / b);
+   return 0;
+  }*/
+
+
---
+++ operators.c
@@ -0,0 +1,17 @@
+ #include<stdio.h> 
+  int main()
+  {
+   int a, b;
+   printf("enter a");
+   scanf("%d", &a);
+   printf("enter b");
+   scanf("%d", &b );
+    
+   int sum=a + b;
+   printf("sum is: %d \n", a + b);
+   printf("diffrence is: %d \n", a - b);
+   printf("multiplication  is: %d \n", a * b);
+   printf("quotient is: %d \n", a / b);
+   return 0;
+  }
+
