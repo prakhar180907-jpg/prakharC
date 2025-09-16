7da86b5477286b2d18029dc7c13647cad0ff2bf7
prakhar180907-jpg <prakhar180907@gmail.com>
2025-09-16 20:37:30 +0530

    Add files via upload

---
+++ Q54.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    int n = 4;
+
+    for (i = 1; i <= n; i++) {
+        space = n - i;
+        for (j = 1; j <= space; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2*i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (i = n - 1; i >= 1; i--) {
+        space = n - i;
+        for (j = 1; j <= space; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2*i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
